7f31ab0b4dc4834cc4e9448449d895ea77145910
Elysiayn <wendy.lemus.wl@gmail.com>
2020-11-22 16:15:18 -0500

    updated html for search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 //basic API call example
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=08aa83b741b9cdfacd456989d2913cdf")
-.then(function(response){
-return response.json()
-}) 
-.then(function(data){
-  console.log(data)
-}) 
+// fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=08aa83b741b9cdfacd456989d2913cdf")
+// .then(function(response){
+// return response.json()
+// }) 
+// .then(function(data){
+//   console.log(data)
+// }) 
+
+var apiKey= "08aa83b741b9cdfacd456989d2913cdf"
+
 
 //weather search function
 var weatherSearch = function (searchQuery) {
-  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&appid=08aa83b741b9cdfacd456989d2913cdf")
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&appid=" + apiKey)
   .then(function(response){
       return response.json()
     }) 
@@ -20,6 +23,6 @@ var weatherSearch = function (searchQuery) {
 
 
 //function call
-weatherSearch("kansas");
+weatherSearch("Jacksonville");
 
 
