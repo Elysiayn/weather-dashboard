802e82fc680fd6eec633facbe7694aa867ab5f3f
Elysiayn <wendy.lemus.wl@gmail.com>
2020-11-22 12:21:46 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+//basic API call example
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=08aa83b741b9cdfacd456989d2913cdf")
+.then(function(response){
+return response.json()
+}) 
+.then(function(data){
+  console.log(data)
+}) 
+
+//weather search function
+var weatherSearch = function (searchQuery) {
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&appid=08aa83b741b9cdfacd456989d2913cdf")
+  .then(function(response){
+      return response.json()
+    }) 
+    .then(function(data){
+      console.log(data)
+    }) 
+}
+
+
+//function call
+weatherSearch("kansas");
+
+
